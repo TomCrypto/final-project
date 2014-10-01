6188f685d5447a8d8e2559a4e85a91fdbd713f78
simjza@gmail.com <simjza@gmail.com>
2014-10-01 14:48:16 +1300

    passes skycolor from m_bar to skybox.frag
    Former-commit-id: 6523fe852c924e532ae8f4e1b4dd2d1b0e9776be

--- src/core/skybox.cpp
+++ src/core/skybox.cpp
@@ -6,13 +6,13 @@ skybox::skybox()
     quad = gluNewQuadric();
 }
 
-void skybox::display(const camera& cam)
+void skybox::display(const camera& cam, glm::vec3 skycolor)
 {
     m_shader.bind();
 
     m_shader.set("view", cam.view(false));
     m_shader.set("proj", cam.proj());
-
+	m_shader.set("skycolor", skycolor);
     //glBegin();
     gluSphere(quad, 100, 10, 10);
     //glEnd();
