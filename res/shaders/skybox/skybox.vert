c14ba76cd06a321c9e668829a43c30160b764841
simjza@gmail.com <simjza@gmail.com>
2014-10-05 00:01:08 +1300

    All Params calculated in skybox.cpp, trouble passing to shader. Still need to code vertex shader.
    Former-commit-id: 406c367179e5408224becb1b9239e07ffce9f4f5

--- res/shaders/skybox/skybox.vert
+++ res/shaders/skybox/skybox.vert
@@ -3,10 +3,22 @@
 uniform mat4 view;
 uniform mat4 proj;
 
-varying vec3 norm;
+varying vec3 extinction;
+varying vec3 inscattering;
+
+uniform float Esun;
+uniform vec3 sunDir;
+uniform vec3 eyePos;
+uniform vec3 betaRay;
+uniform vec3 betaMie;
+uniform vec3 betaRM;
+uniform vec3 oneOverBetaRM;
+uniform vec3 gHG;
 
 void main()
 {
-norm = gl_Normal;
 gl_Position = proj * view * gl_Vertex;
+
+extinction = gl_Vertex.xyz;
+inscattering = vec3(0);
 }
--- src/core/skybox.cpp
+++ src/core/skybox.cpp
@@ -1,4 +1,6 @@
 #include "core/skybox.h"
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
 
 skybox::skybox()
     : m_shader("skybox/skybox.vert", "skybox/skybox.frag")
@@ -16,12 +18,76 @@ void skybox::display(const camera& cam, atmos vars)
     m_shader.set("view", cam.view(false));
 	m_shader.set("proj", cam.proj());
 
+	float pi = 3.14159265358979323846f;
+	float n = .0003f; //regractive index of air
+	float N = 2.545e25; //molecules per unit valume of air
+	float pn = 0.035f; //depolarization factor for air
+	glm::vec3 lambda = glm::vec3(1 / 650e-9f, 1 / 570e-9f, 1 / 475e-9f); // red, green & blue. Note: 650e-9 m = 650nm.
+	float tmp = ((6 + 3 * pn) / (6 - 7 * pn)) * ((pi*pi*std::powf((n*n) - 1.0f, 2)) / 2 * N);
+	//glm::vec3 rayleighMultipier = tmp / (lambda*lambda*lambda*lambda);
+	glm::vec3 betaRayleigh = (tmp / (lambda*lambda*lambda*lambda)) * glm::vec3(16 * pi / 3); //total BetaR
+	/*std::cout << glm::to_string(rayleighMultipier) << "\n";
+	std::cout << glm::to_string(betaRayleigh) << "\n";
+	std::cout << glm::to_string(lambda) << "\n";
+	std::cout << glm::to_string(lambda*lambda) << "\n";
+	std::cout << glm::to_string(lambda*lambda*lambda*lambda) << "\n";
+	std::cout << glm::to_string(glm::vec3(1,2,3) * glm::vec3(6,7,8)) << "\n";*/
+	//exit(0);
+
+	float T = 2.0f; //turbidity
+	float c = (0.6544*T - 0.6510)*1e-16; //concentration factor
+	float v = 4; //Junge's exponent = 4 for the sky model
+
+	glm::vec3 K = glm::vec3(0.685f, 0.679f, 0.670f);
+	tmp = 0.434f*c*pi*(2 * pi)*(2 * pi);
+	//glm::vec3 mieMultiplier = glm::vec3(0.5f)*tmp*lambda*lambda;
+	glm::vec3 betaMie = tmp*K*lambda*lambda;
+
+	//Rayleigh + Mie
+	glm::vec3 betaRM = betaRayleigh+betaMie;
+	glm::vec3 oneOverBetaRM = glm::vec3(1) / betaRM;
+
+	float g = 0.8f; //Henyey Greensteins's G value
+	glm::vec3 HG = glm::vec3(1 - g*g, 1 + g, 2 * g);
+
+
+
+	m_shader.set("betaRay", betaRayleigh);
+	m_shader.set("betaMie", betaMie);
+	m_shader.set("betaRM", betaRM);
+	m_shader.set("oneOverBetaRM", oneOverBetaRM);
+	m_shader.set("gHG", HG);
+
+	//sunDir
+	glm::vec3 sunDir = glm::vec3(glm::cos(glm::radians(vars.theta)), glm::sin(glm::radians(vars.theta))*glm::cos(glm::radians(vars.phi)), glm::sin(glm::radians(vars.theta))*glm::sin(glm::radians(vars.phi)));
+	//calculate colour
+	float fBeta = 0.04608365822050f * T - 0.04586025928522f;
+	float m = 1.0f / (glm::cos(vars.theta) + 0.5f*glm::pow(93.885f - vars.theta / pi*180.0f, -1.253f));
+	glm::vec3 lam = glm::vec3(0.65f, 0.57f, 0.475f); //red green & blue in um
+	float fTauRx = glm::exp(-m*0.008735f*glm::pow(lam.x, -4.08f));
+	float fTauRy = glm::exp(-m*0.008735f*glm::pow(lam.y, -4.08f));
+	float fTauRz = glm::exp(-m*0.008735f*glm::pow(lam.z, -4.08f));
+	float fTauAx = glm::exp(-m*fBeta*glm::pow(lam.x, -1.3f));
+	float fTauAy = glm::exp(-m*fBeta*glm::pow(lam.y, -1.3f));
+	float fTauAz = glm::exp(-m*fBeta*glm::pow(lam.z, -1.3f));
+	glm::vec4 sunColorAndIntensity = glm::vec4(fTauRx*fTauAx, fTauRy*fTauAy, fTauRz*fTauAz, .0f);
+	/*
 	
-    m_shader.set("light_incl", vars.inclination);
+	uniform float Esun;
+	uniform vec3 sunDir;
+	uniform vec3 eyePos;
+	uniform vec3 betaRay;
+	uniform vec3 betaMie;
+	uniform vec3 betaRM;
+	uniform vec3 oneOverBetaRM;
+	uniform vec3 gHG;
+	
+	*/
+    /*m_shader.set("light_incl", vars.inclination);
     m_shader.set("light_lat", vars.latitude);
 	m_shader.set("scatter_strength", vars.InMult);
 	m_shader.set("rayleigh_strength", vars.RayMult);
-	m_shader.set("mie_strength", vars.MieMult);
+	m_shader.set("mie_strength", vars.MieMult);*/
 
     //glBegin();
     gluSphere(quad, 100, 32, 32);
