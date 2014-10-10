f902eda951f866728a0d86bf00ac0cadbb50a68b
simjza@gmail.com <simjza@gmail.com>
2014-10-10 13:32:31 +1300

    fixed some equations skybox

--- src/core/skybox.cpp
+++ src/core/skybox.cpp
@@ -1,3 +1,4 @@
+#include <easylogging.h>
 #include "core/skybox.h"
 #include <glm/gtx/string_cast.hpp>
 #include <iostream>
@@ -21,13 +22,13 @@ void skybox::display(const camera& cam, atmos vars)
 	m_shader.set("eyePos", eyePos);
 
 	float pi = 3.14159265358979323846f;
-	float n = .0003f; //regractive index of air
+	float n = 1.0003f; //regractive index of air
 	float N = 2.545e25; //molecules per unit valume of air
 	float pn = 0.035f; //depolarization factor for air
 	glm::vec3 lambda = glm::vec3(1 / 650e-9f, 1 / 570e-9f, 1 / 475e-9f); // red, green & blue. Note: 650e-9 m = 650nm.
-	float tmp = ((6 + 3 * pn) / (6 - 7 * pn)) * ((pi*pi*std::powf((n*n) - 1.0f, 2)) / 2 * N);
-	glm::vec3 rayleighTheta = tmp / (lambda*lambda*lambda*lambda);
-	glm::vec3 betaRayleigh = rayleighTheta * glm::vec3(16 * pi / 3); //total BetaR
+	float tmp = pi*pi*(n*n - 1.0f)*(n*n - 1.0f)*(6+3*pn)/(6-7*pn)/N;
+	glm::vec3 rayleighTheta = (tmp/2) * (lambda*lambda*lambda*lambda);
+	glm::vec3 betaRayleigh = (8.0f*tmp*pi/3)*(lambda*lambda*lambda*lambda); //total BetaR
 	/*std::cout << glm::to_string(rayleighMultipier) << "\n";
 	std::cout << glm::to_string(betaRayleigh) << "\n";
 	std::cout << glm::to_string(lambda) << "\n";
@@ -43,7 +44,7 @@ void skybox::display(const camera& cam, atmos vars)
 	glm::vec3 K = glm::vec3(0.685f, 0.679f, 0.670f);
 	float Temp = 0.434*c*(2 * pi)*(2 * pi)*0.5f;
 	glm::vec3 mieTheta = Temp*lambda*lambda;
-	Temp = 0.434f*c*pi*(2 * pi)*(2 * pi);;
+	Temp = 0.434f*c*pi*(2 * pi)*(2 * pi);
 	glm::vec3 betaMie = Temp*K*lambda*lambda;
 
 	//Rayleigh + Mie
@@ -68,14 +69,14 @@ void skybox::display(const camera& cam, atmos vars)
 	m_shader.set("sunDir",sunDir);
 	//calculate colour
 	float fBeta = 0.04608365822050f * T - 0.04586025928522f;
-	float m = 1.0f / (glm::cos(glm::radians(vars.theta)) + 0.5f/glm::pow(93.885f - vars.theta, 1.253f));
+	float m = 1.0f / (glm::cos(glm::radians(vars.theta)) + 0.5f / std::pow(93.885f - glm::radians(vars.theta), 1.253f));
 	glm::vec3 lam = glm::vec3(0.65f, 0.57f, 0.475f); //red green & blue in um
-	float fTauRx = glm::exp(-m*0.008735f*glm::pow(lam.x, -4.08f));
-	float fTauRy = glm::exp(-m*0.008735f*glm::pow(lam.y, -4.08f));
-	float fTauRz = glm::exp(-m*0.008735f*glm::pow(lam.z, -4.08f));
-	float fTauAx = glm::exp(-m*fBeta*glm::pow(lam.x, -1.3f));
-	float fTauAy = glm::exp(-m*fBeta*glm::pow(lam.y, -1.3f));
-	float fTauAz = glm::exp(-m*fBeta*glm::pow(lam.z, -1.3f));
+	float fTauRx = glm::exp(-m*0.008735f*std::pow(lam.x, -4.08f));
+	float fTauRy = glm::exp(-m*0.008735f*std::pow(lam.y, -4.08f));
+	float fTauRz = glm::exp(-m*0.008735f*std::pow(lam.z, -4.08f));
+	float fTauAx = glm::exp(-m*fBeta*std::pow(lam.x, -1.3f));
+	float fTauAy = glm::exp(-m*fBeta*std::pow(lam.y, -1.3f));
+	float fTauAz = glm::exp(-m*fBeta*std::pow(lam.z, -1.3f));
 	glm::vec4 sunColorAndIntensity = glm::vec4(fTauRx*fTauAx, fTauRy*fTauAy, fTauRz*fTauAz, 1.0f);
 	/*std::cout << fTauRx << " " << fTauRy << " " << fTauRz << "\n";
 	std::cout << fTauAx << " " << fTauAy << " " << fTauAz << "\n";
