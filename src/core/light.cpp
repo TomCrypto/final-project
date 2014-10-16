f654ea0e99ad2a8399a3fd98c5ae8eb140772b05
TomCrypto <thomas.beneteau@yahoo.fr>
2014-10-17 11:59:46 +1300

    Can now render lights with a transform
    Former-commit-id: a36e9cb6407fa1f187111aa4fa66fac2858b627a

--- src/core/light.cpp
+++ src/core/light.cpp
@@ -12,7 +12,8 @@ light_renderer::~light_renderer()
 }
 
 void light_renderer::display(const camera& camera,
-                             const std::vector<light>& lights)
+                             const std::vector<light>& lights,
+                             const glm::mat4& transform)
 {
     m_shader.bind();
     m_shader.set("proj", camera.proj());
@@ -21,7 +22,7 @@ void light_renderer::display(const camera& camera,
         bool fixed = (light.position.w == 0);
         m_shader.set("view", camera.view(!fixed));
         m_shader.set("light_color", light.intensity);
-        m_shader.set("light_pos", (glm::vec3)light.position);
+        m_shader.set("light_pos", (glm::vec3)(transform * light.position));
 
         if (fixed) {
             glDisable(GL_DEPTH_TEST);
